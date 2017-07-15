2015bd158ec4a11102cd1e5f25cb9255661921dc
Nathan Simpson <nathansimpson5@gmail.com>
2017-07-15 10:09:44 -0400

    add levels to cat

--- js/app.js
+++ js/app.js
@@ -1,13 +1,22 @@
 var ViewModel = function() {
 	this.clickCount = ko.observable(0);
-	this.name = ko.observable('Nipsy');
-	this.level = ko.observable('Newborn');
+	this.name = ko.observable('lil boat');
 	this.imgSrc = ko.observable('img/cat1.jpg');
 	this.imgAttribution = ko.observable('http://google.com');
 
 	this.incrementCounter = function() {
 		this.clickCount(this.clickCount() + 1);
 	};
+
+	this.level = ko.computed(function(){
+		if (this.clickCount() < 10) {
+			return "newborn";
+		} else if (this.clickCount() < 20) {
+			return "infant";
+		} else {
+			return "teen";
+		}
+	}, this);
 }
 
 ko.applyBindings(new ViewModel());
