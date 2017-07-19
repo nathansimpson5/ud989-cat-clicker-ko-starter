7ecc794908c733d86251babce0e859b63007aacf
Nathan Simpson <nathansimpson5@gmail.com>
2017-07-19 18:09:46 -0400

    refactor code to knockoutJS

--- js/app.js
+++ js/app.js
@@ -1,12 +1,48 @@
-var ViewModel = function() {
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable('lil boat');
-	this.imgSrc = ko.observable('img/cat1.jpg');
-	this.imgAttribution = ko.observable('http://google.com');
+var initialCats = [
+	{
+		clickCount: 0,
+		name: 'nipsy',
+		imgSrc: 'img/cat.jpg',
+		nicknames: ['nips']
+	},
+	{
+		clickCount: 0,
+		name: 'lil boat',
+		imgSrc: 'img/cat1.jpg',
+		nicknames: ['chunks', 'bb', 'thumble']
+	},
+	{
+		clickCount: 0,
+		name: 'muffins',
+		imgSrc: 'img/cat2.jpg',
+		nicknames: ['blue boy']
+	},
+	{
+		clickCount: 0,
+		name: 'cranky',
+		imgSrc: 'img/cat3.jpg',
+		nicknames: ['meanies dont get nicknames']
+	},
+	{
+		clickCount: 0,
+		name: 'lego',
+		imgSrc: 'img/cat4.jpg',
+		nicknames: ['go-go']
+	},
+	{
+		clickCount: 0,
+		name: 'terry',
+		imgSrc: 'img/cat5.jpg',
+		nicknames: ['nice boy']
+	},
+];
 
-	this.incrementCounter = function() {
-		this.clickCount(this.clickCount() + 1);
-	};
+
+var Cat = function(data) {
+	this.clickCount = ko.observable(data.clickCount);
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
+	this.nicknames = ko.observableArray(data.nicknames);
 
 	this.level = ko.computed(function(){
 		if (this.clickCount() < 10) {
@@ -17,6 +53,27 @@ var ViewModel = function() {
 			return "teen";
 		}
 	}, this);
+}
+
+var ViewModel = function() {
+	var self = this;
+
+	this.catList = ko.observableArray([]);
+
+	initialCats.forEach(function(catItem){
+		self.catList.push (new Cat(catItem) );
+	});
+
+	this.currentCat = ko.observable(this.catList()[0]);
+
+	this.incrementCounter = function() {
+		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+	};
+
+	this.setCurrentCat = function(clickedCat) {
+		self.currentCat(clickedCat);
+	}
+
 }
 
 ko.applyBindings(new ViewModel());
